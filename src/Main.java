032de96618ea2822ba02c17b838ad6a7acf3c302
samuel Galliani-Royer <samuel.galliani-royer@edu.esiee-it.fr>
2024-10-07 14:13:32 +0200

    modif main 2

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
         System.out.println("modif master");
+        System.out.println("modif master 2");
 
-        }
+    }
     }
